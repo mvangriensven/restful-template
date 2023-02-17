82b936fba9dd1acd9d531fbc43cdcb00ed73ad02
Marius van Griensven <mariusvangriensven@gmail.com>
2023-02-17 18:54:49 +0100

    Removes X-Powered-By

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ import usersRoutes from './routes/users.js';
 const app   = express();
 const PORT  = 5000;
 
+app.disable('x-powered-by');
+
 app.use(bodyParser.json());
 
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
+    res.removeHeader('X-Powered-By');
     res.send('RESTful API Template.');
 });
 
